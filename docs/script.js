a985ba7aa362a66378b8901c65edf7b9bb72944c
Pankaj Patel <pankajpatel@users.noreply.github.com>
2017-01-03 22:10:03 +0100

    viewer with country filter (#128)

---
+++ docs/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(e) {
+	var companies = [];
+	var countries = {};
+	$.get('../README.md', function(response) {
+		var data = response.split('\n| Company Name | Location |\n| --- | --- |\n')
+		$('#placeholder').html(markdown.toHTML( data[0] ));
+
+		data[1].split('\n').forEach(function(item) {
+			var row = item.split(' | ');
+			var company = {};
+			var trClasses = ['company'];
+			company.title = markdown.toHTML(row[0].replace(/\|/g, '').trim());
+			company.countries = row[1] ? row[1].replace(/\|/g, '').trim().split('; ') : [];
+
+			company.countries.map(function(item) {
+				var place = {
+					name: item,
+					class: item.replace(/[\W]/g, '-')
+				}
+				trClasses.push(place.class);
+				countries[place.class] = place;
+			});
+			companies.push(company);
+			$('#list > tbody').append('<tr class="'+trClasses.join(' ')+'"><td>'+ company.title
+				+'</td><td><p>'+
+				company.countries.join('</p><p>')
+				+'</p></td></tr>')
+		});
+		$('#filter').append('<option value="company">All</option>');
+		Object.keys(countries).map(function(item) {
+			$('#filter').append('<option value="'+countries[item].class+'">'+countries[item].name+'</option>')
+		})
+	})
+
+	$('#filter').on('change', function(e) {
+		$('.company')
+			.hide();
+		$('.company.'+$(this).val())
+			.show();
+	})
+})
